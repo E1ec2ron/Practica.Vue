55def949a85f2e828c308eb201cf1b5264095068
public <public@copp.local>
2025-02-12 16:00:08 +0700

    6 commit

--- vue/js/main.js
+++ vue/js/main.js
@@ -67,3 +67,105 @@ Vue.component('product-review', {
         }
     }
 });
+
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template:`
+   <div class="product">
+      <div class="product-image">
+         <img :alt="altText" :src="image"/>
+      </div>
+
+
+      <div class="product-info">
+         <h1>{{ title }}</h1>
+         <p v-if="inStock">In Stock</p>
+         <p v-else>Out of Stock</p>
+         <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+         </ul>         
+         <p>Shipping: {{ shipping }}</p>
+         
+         <div class="color-box" 
+         v-for="(variant, index) in variants" 
+         :key="variant.variantId" 
+         :style="{ backgroundColor:variant.variantColor }" 
+         @mouseover="updateProduct(index)">
+         </div>
+         
+         <button @click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">Add to cart</button>
+      </div>
+         
+         <product-tabs :reviews="reviews"></product-tabs>
+   </div>
+`,
+    data(){
+        return{
+            product: "Socks",
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+            altText: "A pair of socks",
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: 'green',
+                    variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'blue',
+                    variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                    variantQuantity: 0
+                }
+            ],
+            reviews: []
+        };
+    },
+
+    methods: {
+        addToCart() {
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
+        },
+
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        },
+    },
+
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
+        },
+
+        shipping() {
+            if (this.premium){
+                return "Free";
+            } else {
+                return 2.99;
+            }
+        }
+    },
+
+    mounted() {
+        eventBus.$on("review-submitted", productReview => {
+            this.reviews.push(productReview);
+        });
+    },
+
+});
